63f025deaabb3bd1ff9efbf2e4b8e2f9138ce1fc
Michael Deveau <michael_deveau@yahoo.com>
2025-01-15 18:08:12 -0800

    Added group methods

--- models/group.js
+++ models/group.js
@@ -33,7 +33,33 @@ const GroupSchema = new Schema(
 );
 
 GroupSchema.methods.updateGroup = function (name, description) {
-  
-}
+  this.name = name;
+  this.description = description;
+  this.save();
+};
+
+GroupSchema.methods.addMember = function (user) {
+  if (!this.members.some((member) => member.userId === user.userId)) {
+    this.members.push(user.username);
+    return this.save();
+  }
+  return;
+};
+
+GroupSchema.methods.updateMember = function (user, role) {
+  const memberIndex = this.members.findIndex((member) => {
+    return member.user.username.toString() === user.username.toString();
+  });
+  this.members[memberIndex].role = role;
+  return this.save();
+};
+
+GroupSchema.methods.removeMember = function (user) {
+  const memberIndex = this.members.findIndex((member) => {
+    return member.user.username.toString() === user.username.toString();
+  });
+  this.members.pull(this.members[memberIndex]);
+  return this.save();
+};
 
 export const Group = mongoose.model("Group", GroupSchema);
